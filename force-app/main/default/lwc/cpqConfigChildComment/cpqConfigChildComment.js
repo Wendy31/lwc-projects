0fe1c33ab6689f4e717602fa6ff1fb2426fa0e8b
Wendy <wendychung31@gmail.com>
2022-06-10 17:51:05 +0100

    completed text editor

--- force-app/main/default/lwc/cpqConfigChildComment/cpqConfigChildComment.js
+++ force-app/main/default/lwc/cpqConfigChildComment/cpqConfigChildComment.js
@@ -1,12 +1,19 @@
 import { LightningElement, track } from 'lwc';
 
 export default class CpqConfigChildComment extends LightningElement {
+    myVal = '<b>Hello!</b>';
 
     connectedCallback(){
-        this.format(value);
+        //this.format(value);
+       // this.handleChange(e);
     }
 
-    format(value) {
+   /* format(value) {
         this.template.execCommand(command, showUI, value);
+    } 
+    */
+
+    handleChange(event) {
+        this.myVal = event.target.value;
     }
 }
